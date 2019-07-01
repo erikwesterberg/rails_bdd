5ff31f2138eaeb5a04620aefe21c65be1d721827
erka <erik.westerberg@hotmail.se>
2019-07-01 13:05:42 +0200

    Disable generation of helpers in applictions file

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,16 @@ Bundler.require(*Rails.groups)
 
 module RailsDemo
   class Application < Rails::Application
-    config.load_defaults 5.2
-    config.generators.system_tests = nil
+    config.load_defaults 5.1
+
+  # Disable generation of helpers, javascripts, css, and view, helper, routing and controller specs
+  config.generators do |generate|
+    generate.helper false
+    generate.assets false
+    generate.view_specs false
+    generate.helper_specs false
+    generate.routing_specs false
+    generate.controller_specs false
+    generate.system_tests false
   end
 end
