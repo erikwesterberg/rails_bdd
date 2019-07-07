88f7c620cdf0e4a92cd88a80868d9ddaf7a20234
erka <erik.westerberg@hotmail.se>
2019-07-07 10:33:08 +0200

    new article link created and form for title and content

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -1,13 +1,30 @@
+
 When("I visit the site") do
-    visit root_path
-  end
-  
-Then("I should see {string}") do |content|
-    expect(page).to have_content content
-  end
+  visit root_path
+end
 
 Given("the following articles exists") do |table|
-    table.hashes.each do |article|
-      FactoryBot.create(:article, article)
-    end
+  table.hashes.each do |article|
+    FactoryBot.create(:article, article)
   end
+end
+
+Given("I click {string}") do |element|
+  click_on element
+end
+
+When("I fill in {string} with {string}") do |field, content|
+  fill_in field, with: content
+end
+
+Then("I should see {string}") do |content|
+  expect(page).to have_content content
+end
+
+When("I click {string} button") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be on {string} page") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
